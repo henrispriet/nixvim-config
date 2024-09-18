2562c2561d74ba6e90723e0356b5f6de3a725ba2
henrispriet <henri.spriet@gmail.com>
2024-09-18 10:32:31 +0200

    coq stfu

--- plugins/lsp/coq.nix
+++ plugins/lsp/coq.nix
@@ -4,17 +4,5 @@
 
   # might turn this off
   plugins.coq-nvim.settings.completion.always = true;
-
-  # auto COQnow
-  autoGroups.auto_coq = {};
-  autoCmd = [
-    {
-      desc = "auto activate coq.nvim";
-      event = ["BufAdd"];
-      # possible filetypes: https://github.com/neovim/neovim/blob/master/runtime/lua/vim/filetype.lua
-      pattern = ["*"];
-      group = "auto_coq";
-      command = "COQnow";
-    }
-  ];
+  plugins.coq-nvim.settings.auto_start = "shut-up";
 }
