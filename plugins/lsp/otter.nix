ad46b3700038a1184754ce799915f2d9584fc6f3
henrispriet <henri.spriet@gmail.com>
2024-09-22 23:55:35 +0200

    otter.autoActivate was added to nixvim
    default true

--- plugins/lsp/otter.nix
+++ plugins/lsp/otter.nix
@@ -8,21 +8,4 @@
   # necessary for some otter.nvim functionality
   plugins.treesitter.enable = true;
   plugins.treesitter.settings.highlight.enable = true;
-
-  # auto otter.activate()
-  autoGroups.auto_otter = {};
-  autoCmd = [
-    {
-      desc = "auto activate otter.nvim";
-      event = ["FileType"];
-      # possible filetypes: https://github.com/neovim/neovim/blob/master/runtime/lua/vim/filetype.lua
-      pattern = ["nix"];
-      group = "auto_otter";
-      callback.__raw = /* lua */ ''
-	function()
-	  require("otter").activate()
-	end
-      '';
-    }
-  ];
 }
