e66f36072bb230a260fd92bd21ea3f5e2e648e8b
henrispriet <henri.spriet@gmail.com>
2024-09-23 01:41:58 +0200

    add keymap to quickly insert (png) image into markdown

--- plugins/md-notes.nix
+++ plugins/md-notes.nix
@@ -30,11 +30,36 @@
     };
   };
 
-  # TODO: keymap to
-  # - vim.input to choose filename
-  # - create file (how to create empty image???)
-  # - open file in pkgs.drawing
-  # - if file was saved to, then insert markdown link to file, else do nothing
   keymaps = [
+    {
+      options.desc = "Quick insert image";
+      key = "<leader>md";
+      action.__raw = /* lua */ ''
+        function()
+          vim.ui.input("filename", function(filename)
+            if filename == nil then
+              -- return if input is canceled
+              return
+            elseif filename == "" then
+              -- generater random name if none is given
+              local fp = io.popen("base32 /dev/urandom | head -c 15")
+              filename = fp:read("*all")
+              fp:close()
+            end
+            filename = filename..".png"
+
+            -- write link to file to buffer
+            vim.cmd("r!echo \\!["..filename.."]")
+
+            -- create blank file
+            -- https://superuser.com/a/294948
+            os.execute("${pkgs.imagemagick}/bin/magick -size 1000x600 xc:white "..filename)
+
+            -- open drawing
+            os.execute("${pkgs.drawing}/bin/drawing --new-tab "..filename.." &")
+          end)
+        end
+      '';
+    }
   ];
 }
