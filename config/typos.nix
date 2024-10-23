4593778828995e278eb780c4f38b3489d551c478
henrispriet <henri.spriet@gmail.com>
2024-10-23 14:20:28 +0200

    more complete typo corrections for wqa

--- config/typos.nix
+++ config/typos.nix
@@ -1,4 +1,4 @@
-{...}: {
+{ lib, ... }: {
   # Correct common typos
 
   keymaps = [
@@ -13,14 +13,25 @@
     }
   ];
 
-  userCommands = {
-    Q.command = "q";
-    Q.bang = true;
-    Wq.command = "wq";
-    Wq.bang = true;
-    WQ.command = "wq";
-    WQ.bang = true;
-    W.command = "w";
-    W.bang = true;
-  };
+  userCommands = let
+    inherit (lib) genAttrs toUpper toLower;
+
+    # FIXME: command must start with uppercase
+    alternateCase = cmd: lib.mapCartesianProduct
+      ({ w ? "", q ? "", a ? "" }: w + q + a)
+      (genAttrs
+        (lib.stringToCharacters cmd)
+        (c: [ (toLower c) (toUpper c) ])
+      );
+
+    mkLowerAlias = cmd: {
+      command = toLower cmd;
+      bang = true;
+    };
+  in
+    genAttrs (lib.concatMap
+        alternateCase
+        [ "w" "q" "wq" "wa" "qa" "wqa" ]
+      )
+      mkLowerAlias;
 }
