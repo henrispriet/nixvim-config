f9eef5dab95391af75440434d0be8812aa1341cb
henrispriet <henri.spriet@gmail.com>
2024-12-21 17:28:28 +0100

    add comments to lsp servers showing language

--- plugins/lsp/servers.nix
+++ plugins/lsp/servers.nix
@@ -13,15 +13,22 @@
     };
 
     servers = {
+			# lua
       lua_ls.enable = true;
+			# nix (broken)
       nixd.enable = true;
 
+			# html
       html.enable = true;
+			# javascript/typescript
       ts_ls.enable = true;
+			# SQL
       # needs configing with db connection
       # sqls.enable = true;
 
+			# bash
       bashls.enable = true;
+			# markdown
       marksman.enable = true;
     };
   };
